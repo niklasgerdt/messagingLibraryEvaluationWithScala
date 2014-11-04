e4861c5696c05aaac07a298b8768f848dc2d6dcb
Niklas Gerdt <gerdtni@gmail.com>
2014-11-04 21:37:09 +0200

    scala to separate repo

---
+++ src/test/scala/pubTest.scala
@@ -0,0 +1,16 @@
+package eu.route20.hft.pub
+
+import eu.route20.hft.test.BaseTest
+import grizzled.slf4j.Logging
+import org.scalatest._
+import pubsub.Notification
+
+class DummyPubTest extends BaseTest {
+
+  class Mock
+
+//  "DummyPub" should "do nothing" in {
+//    val m = new Mock with DummyPub
+//    m.pub(Notification(None, ""))
+//  }
+}
